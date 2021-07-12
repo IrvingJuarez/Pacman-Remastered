7d3b2bc3fb3e82b99113b4876da957698966a1db
Irving Juarez <irvingjuarez274@gmail.com>
2021-07-12 16:31:59 -0500

    The Error404 page was created and the development mode was setted in webpack

---
+++ src/pages/Error404.js
@@ -0,0 +1,13 @@
+const Content = () => {
+    const view = `
+        <section class="ContentError404">
+            <h1>Error 404</h1>
+            <span class="NotFound">Pacman not found</span>
+            <div class="GhostContainer"></div>
+        </section>
+    `;
+
+    return view;
+}
+
+export default Content;
