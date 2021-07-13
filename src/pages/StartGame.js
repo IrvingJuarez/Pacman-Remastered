f4d134d3293f15c6900175760341c979179ab930
Irving Juarez <irvingjuarez274@gmail.com>
2021-07-12 23:47:11 -0500

    Logo image added successfully

--- src/pages/StartGame.js
+++ src/pages/StartGame.js
@@ -3,8 +3,10 @@ import styles from "../styles/StartGame.css"
 const StartGame = () => {
     const view = `
         <section class="ContentStartGame">
-            <div class="LogoContainer"></div>
-            <span class="Subtitle">REMASTERED</span>
+            <div class="Logo">
+                <div class="LogoImage"></div>
+                <span class="Subtitle">REMASTERED</span>
+            </div>
             <button class="PlayButton">
                 Play Game!
             </button>
