f9b254c81c0bac7e89f28ef6fc5690cfa8adbade
Irving Juarez <irvingjuarez274@gmail.com>
2021-07-16 17:43:01 -0500

    Testing the js files according to each layout

--- src/utils/getLayout.js
+++ src/utils/getLayout.js
@@ -3,23 +3,33 @@ import grid from "./grid"
 const getLayout = () => {
     const head = document.head
     const link = document.createElement("link")
+    const body = document.body
+    const script = document.createElement("script")
+
     const screenWidth = screen.width;
     const screenHeight = screen.height;
-    let source;
+    let source, js;
 
     if(screenWidth >= 320 && screenWidth < 375){
         source = "../src/styles/designs/320.css"
+        js = "../src/javascript/320.js"
     }else if ((screenWidth >= 375 && screenWidth < 1210) && screenHeight < 736){
         source = "../src/styles/designs/375.css"
+        js = "../src/javascript/375.js"
     }else if ((screenWidth >= 375 && screenWidth < 1210) && screenHeight >= 737){
         source = "../src/styles/designs/higher.css"
+        js = "../src/javascript/higher.js"
     }else if(screenWidth >= 1210){
         source = "../src/styles/designs/desktop.css"
+        js = "../src/javascript/desktop.js"
     }
 
     link.rel = "stylesheet"
     link.href = source
     head.appendChild(link)
+
+    script.src = js
+    body.appendChild(script)
 
     setTimeout(() => {
         grid()
