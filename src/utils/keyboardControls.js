698545101af2351ee17dd2cea4ede91d7328f4c6
Irving Juarez <irvingjuarez274@gmail.com>
2021-07-17 13:43:55 -0500

    Now the pacman can move all around freely. Of course cannot go to the walls and to the border of the boardGame

--- src/utils/keyboardControls.js
+++ src/utils/keyboardControls.js
@@ -1,25 +1,52 @@
+const movement = (key, y, x, boardGame) => {
+    switch(key){
+        case "ArrowLeft":
+            x--
+        break;
+        case "ArrowRight":
+            x++
+        break;
+        case "ArrowUp":
+            y--
+        break;
+        case "ArrowDown":
+            y++
+        break;
+    }
+
+    return boardGame.childNodes[y].childNodes[x]
+    
+}
+
 const keyboardControls = (boardGame, pacman, pacmanContainer, row, column) => {
     document.addEventListener("keydown", (event) => {
-        var node = pacmanContainer.childNodes[0];
-        pacmanContainer.removeChild(node)
+        var expectedRow = row, expectedColumn = column, expectedContainer;
 
-        switch(event.key){
-            case "ArrowLeft":
-                column--
-            break;
-            case "ArrowRight":
-                column++
-            break;
-            case "ArrowUp":
-                row--
-            break;
-            case "ArrowDown":
-                row++
-            break;
+        expectedContainer = movement(event.key, expectedRow, expectedColumn, boardGame)
+        let datasetValue = expectedContainer.dataset.value
+
+        if(datasetValue === undefined){
+            var node = pacmanContainer.childNodes[0];
+            pacmanContainer.removeChild(node)
+
+            switch(event.key){
+                case "ArrowLeft":
+                    column--
+                break;
+                case "ArrowRight":
+                    column++
+                break;
+                case "ArrowUp":
+                    row--
+                break;
+                case "ArrowDown":
+                    row++
+                break;
+            }
+
+            pacmanContainer = boardGame.childNodes[row].childNodes[column]
+            pacmanContainer.appendChild(pacman)
         }
-
-        pacmanContainer = boardGame.childNodes[row].childNodes[column]
-        pacmanContainer.appendChild(pacman)
     })
 }
 
